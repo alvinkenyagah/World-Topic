336c3508aaae9d2ddf9b23313b290cc0c21bb090
alvinkenyagah <alvin47k@gmail.com>
2023-04-06 19:52:13 +0300

    Add scroll to top button

--- src/theme.js
+++ src/theme.js
@@ -13,18 +13,3 @@ function dark() {
 }
 
 
-
-let backToTop = document.querySelector("#back-to-top");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    backToTop.classList.add("show");
-  } else {
-    backToTop.classList.remove("show");
-  }
-});
-
-backToTop.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
-
