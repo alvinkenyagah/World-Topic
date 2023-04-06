212b24e3f3ff0e411c0bd12eb19eaf959d28637c
alvinkenyagah <alvin47k@gmail.com>
2023-04-06 08:57:34 +0300

    Add theme script for light and dark mode

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+function dark() {
+    let darkMode = document.querySelector('.dark');
+    if (darkMode.style.backgroundColor === 'black' && darkMode.style.color === 'white') {
+      darkMode.style.backgroundColor = 'white';
+      darkMode.style.color = 'black';
+    } else {
+      darkMode.style.backgroundColor = 'black';
+      darkMode.style.color = 'white';
+    }
+  }
